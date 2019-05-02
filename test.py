1d2af8ba3b8008f85e908ee134b15d11a7080105
Rmsharks4 <rmsharks4@gmail.com>
2019-05-02 21:35:35 +0500

    init

---
+++ test.py
@@ -0,0 +1,70 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from pyimagesearch.minivggnet import MiniVGGNet
+from sklearn.metrics import classification_report
+from keras.optimizers import SGD
+from keras.datasets import fashion_mnist
+from keras.utils import np_utils
+from keras.models import load_model
+from keras import backend as K
+from imutils import build_montages
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+NUM_EPOCHS = 25
+INIT_LR = 1e-2
+BS = 32
+
+print("Loading Fashion MNIST...")
+((trainX, trainY), (testX, testY)) = fashion_mnist.load_data()
+
+if K.image_data_format() == "channels_first":
+	trainX = trainX.reshape((trainX.shape[0], 1, 28, 28))
+	testX = testX.reshape((testX.shape[0], 1, 28, 28))
+else:
+	trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+	testX = testX.reshape((testX.shape[0], 28, 28, 1))
+ 
+trainX = trainX.astype("float32") / 255.0
+testX = testX.astype("float32") / 255.0
+
+trainY = np_utils.to_categorical(trainY, 10)
+testY = np_utils.to_categorical(testY, 10)
+
+labelNames = ["top", "trouser", "pullover", "dress", "coat",
+	"sandal", "shirt", "sneaker", "bag", "ankle boot"]
+
+model = load_model('model.h5')
+model.summary()
+	
+images = []
+
+for i in np.random.choice(np.arange(0, len(testY)), size=(16,)):
+
+	probs = model.predict(testX[np.newaxis, i])
+	prediction = probs.argmax(axis=1)
+	label = labelNames[prediction[0]]
+ 
+	if K.image_data_format() == "channels_first":
+		image = (testX[i][0] * 255).astype("uint8")
+ 
+	else:
+		image = (testX[i] * 255).astype("uint8")
+
+	color = (0, 255, 0)
+
+	if prediction[0] != np.argmax(testY[i]):
+		color = (0, 0, 255)
+ 
+	image = cv2.merge([image] * 3)
+	image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_LINEAR)
+	cv2.putText(image, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+		color, 2)
+
+	images.append(image)
+
+montage = build_montages(images, (96, 96), (4, 4))[0]
+cv2.imshow("Testing Model", montage)
+cv2.waitKey(0)
